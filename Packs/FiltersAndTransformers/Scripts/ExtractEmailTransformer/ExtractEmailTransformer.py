3876c58ecc9b02618735c8c49c5d03dfe67d2872
esharf <esahrf@paloaltonetworks.com>
2022-02-17 14:25:45 +0200

    fix email regex

--- Packs/FiltersAndTransformers/Scripts/ExtractEmailTransformer/ExtractEmailTransformer.py
+++ Packs/FiltersAndTransformers/Scripts/ExtractEmailTransformer/ExtractEmailTransformer.py
@@ -2,11 +2,22 @@ import demistomock as demisto
 from CommonServerPython import *
 
 
+def regex_create():
+    domain = r'(?:[.-]?[a-zA-Z\d]+)+'
+    general_local_part_allowed = r"\d\w!#$%&'*+\-/=?^_`{|}~"
+    lacal_part = rf"(?:[{general_local_part_allowed}](?:[{general_local_part_allowed}]|\.(?!\.))*)"
+
+    return rf'(?:^|(?<=[<\s])){lacal_part}@{domain}(?:(?=[>\s])|$)'
+
+
+EMAIL_REGEX = re.compile(regex_create())
+
+
 def main():
     try:
         list_results = []
         for val in argToList(demisto.args().get('value')):
-            list_results.extend(list(re.findall(emailRegex, val)))
+            list_results.extend(list(re.findall(EMAIL_REGEX, val)))
         return_results(list_results)
     except Exception as error:
         return_error(str(error), error)
--- Packs/FiltersAndTransformers/Scripts/ExtractEmailTransformer/ExtractEmailTransformer_test.py
+++ Packs/FiltersAndTransformers/Scripts/ExtractEmailTransformer/ExtractEmailTransformer_test.py
@@ -2,6 +2,46 @@ from ExtractEmailTransformer import *
 import pytest
 
 
+valid_address_str = """simple@example.com
+very.common@example.com
+disposable.style.email.with+symbol@example.com
+other.email-with-hyphen@example.com
+fully-qualified-domain@example.com
+user.name+tag+sorting@example.com
+x@example.com
+example-indeed@strange-example.com
+example@s.example
+user%example.com@example.org
+user-@example.org
+test <test@test.com>"""
+
+
+valid_address = [
+    "simple@example.com",
+    "very.common@example.com",
+    "disposable.style.email.with+symbol@example.com",
+    "other.email-with-hyphen@example.com",
+    "fully-qualified-domain@example.com",
+    "user.name+tag+sorting@example.com",
+    "x@example.com",
+    "example-indeed@strange-example.com",
+    "example@s.example",
+    "user%example.com@example.org",
+    "user-@example.org",
+    "test@test.com",
+]
+
+
+data_test_regex_create = [
+    (valid_address_str, valid_address),
+]
+
+
+@pytest.mark.parametrize('input_str, email_address', data_test_regex_create)
+def test_regex_create(input_str, email_address):
+    assert list(re.findall(EMAIL_REGEX, input_str)) == email_address
+
+
 data_test_main = [
     ({'value': 'test'}, []),
     ({'value': 'test@test.com'}, ['test@test.com']),
@@ -12,7 +52,6 @@ data_test_main = [
 @pytest.mark.parametrize('args, command_outputs', data_test_main)
 def test_main(args, command_outputs, mocker):
     mocker.patch.object(demisto, 'args', return_value=args)
-    mocker.patch('ExtractEmailTransformer.execute_command', return_value=command_outputs)
     results_mocker = mocker.patch('ExtractEmailTransformer.return_results')
     main()
     results_mocker.args[0] == command_outputs
